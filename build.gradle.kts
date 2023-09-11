5a1c4e0540efd32151938dcff97c1e154eb8ec45
Иван Тимашков <timscriptov@mail.ru>
2023-09-11 19:11:37 +0500

    Create desktop project

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.mcal"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven { url = uri("https://www.jitpack.io" ) }
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.material3)
+
+                implementation(project(":library"))
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "nmmp"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ library/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    java
+    id("maven-publish")
+    kotlin("jvm")
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://www.jitpack.io" ) }
+}
+
+dependencies {
+    implementation("com.github.TimScriptov:apkparser:1.2.4")
+    implementation("com.github.TimScriptov:preferences:1.0.1")// {
+        //exclude(module = "kotlinx-serialization-core-jvm")
+        //exclude(module = "kotlinx-serialization-json-jvm")
+    //}
+    implementation("com.android.tools.smali:smali-dexlib2:3.0.3")
+    implementation("org.jetbrains:annotations:24.0.1")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.ow2.asm:asm:9.5")
+    implementation("com.android.tools:r8:8.1.56")
+    implementation("com.android:zipflinger:8.1.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.mcal"
+            artifactId = "nmmp"
+            version = "1.2.3"
+
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
+    }
+}
